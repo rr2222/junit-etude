5ac602cb2d13c59a60413918b491a3f1e4e4766f
unknown <ruither.carvalho@gmail.com>
2021-04-02 00:24:47 -0300

    teste com mock

--- src/test/java/teste/CalculadoraTest.java
+++ src/test/java/teste/CalculadoraTest.java
@@ -3,6 +3,8 @@ package teste;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
     @Test
@@ -10,5 +12,14 @@ public class CalculadoraTest {
         Calculadora cal = new Calculadora();
         int soma = cal.somar("1+1+2");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testeSomarComMock(){
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);
+        int resultado = calculadora.somar("1+2");
+        assertEquals(10, resultado);
     }
 }
